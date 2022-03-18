d2106e58fcf64e49e307efe5a0724ff260c5bef5
Jessica Edmonds <jessica1329b@gmail.com>
2022-03-18 14:49:01 -0400

    variables exercise

---
+++ Variables Exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string catName = "MiMi";
+            int catAge = 3;
+            char myCat = 'M';
+            bool hasATail = true;
+            double catsWeight = 6.0;
+            decimal myDecimal = 6.0m;
+            Console.WriteLine($"My cats name is  {catName}, He is {catAge} years old ");
+        }
+    }
+}
